a0b09c09312cade5ca42a22a7497a33483027ae2
KrisNathan <kristophernathanael2005@gmail.com>
2025-07-17 21:11:03 +0700

    fix: sidebar buttons light theme color fix

--- src/components/IconBtn.tsx
+++ src/components/IconBtn.tsx
@@ -10,7 +10,7 @@ interface IconBtnProps {
 
 export default function IconBtn({ icon, label, className = '', disabled = false, onClick }: IconBtnProps) {
   return (
-    <button className={`active:animate-(--anim-click) cursor-pointer select-none ${className} flex flex-row gap-3 px-4 py-3 rounded-2xl hover:bg-bg-secondary-hover transition-colors duration-200`} onClick={onClick} aria-label={label} tabIndex={0} disabled={disabled}>
+    <button className={`active:animate-(--anim-click) cursor-pointer select-none ${className} flex flex-row gap-3 px-4 py-3 rounded-2xl hover:bg-bg-secondary-hover transition-colors duration-200 text-text-default`} onClick={onClick} aria-label={label} tabIndex={0} disabled={disabled}>
       {icon}
       {label}
     </button>
