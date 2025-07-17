4e4870c17e80f300cc8d5294edc06c01d93b0356
KrisNathan <kristophernathanael2005@gmail.com>
2025-07-17 15:38:22 +0700

    feat(style): improved navitem appearance when using unicode emoji

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,4 +1,4 @@
-import './NavItem.css';
+import './NavItem.css'
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
@@ -8,14 +8,16 @@ interface NavItemProps {
   icon: React.ReactNode;
 }
 
-export default function NavItem({to, title, icon }: NavItemProps) {
+export default function NavItem({ to, title, icon }: NavItemProps) {
   const location = useLocation();
   const isActive = location.pathname === to;
 
   return (
-    <Link to={to} className={`nav-item active:animate-(--anim-click) select-none ${isActive ? "active" : ""}`}>
+    <Link to={to} className={`active:animate-(--anim-click) cursor-pointer select-none ${isActive ? "active" : ""} flex flex-row gap-3 py-3 px-4 rounded-2xl hover:bg-bg-secondary-hover transition-colors duration-200`}>
       {icon}
-      {title}
+      <div>
+        {title}
+      </div>
     </Link>
   )
 }
