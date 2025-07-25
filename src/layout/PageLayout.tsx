c6b4d0214d14931e0c4abec477b4e335c9a49ab1
KrisNathan <kristophernathanael2005@gmail.com>
2025-07-25 19:10:15 +0700

    fix: better scrolling for content inside PageLayout, keeps titlebar on top

--- src/layout/PageLayout.tsx
+++ src/layout/PageLayout.tsx
@@ -3,9 +3,13 @@ import Titlebar from "../components/Titlebar";
 
 export default function PageLayout() {
   return (
-    <div className="flex flex-col h-full w-full p-4 gap-4">
-      <Titlebar />
-      <Outlet />
+    <div className="flex flex-col h-full w-full">
+      <div className="p-4">
+        <Titlebar />
+      </div>
+      <div className="flex-1 overflow-y-auto px-4">
+        <Outlet />
+      </div>
     </div>
   );
 }
