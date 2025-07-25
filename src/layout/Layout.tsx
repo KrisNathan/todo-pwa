8a16c2e66918a6e1fb98e24d8be1917a67b4e614
KrisNathan <kristophernathanael2005@gmail.com>
2025-07-25 18:22:48 +0700

    fix: add overflow-y-auto class to Layout to handle rare case of overflow

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -5,7 +5,7 @@ import { Outlet } from 'react-router-dom'
 export default function Layout() {
   return (
     <div className='w-full h-full flex flex-col'>
-      <div className='flex-1'>
+      <div className='flex-1 overflow-y-auto'>
         <Outlet />
       </div>
       <MobileNavbar />
